9e971c366427d37f7cf38b767179e3296c4ba427
Garrukh <kacper.maciejewski@gmail.com>
2023-12-19 15:24:56 +0100

    indexing fixed

--- backup.tf
+++ backup.tf
@@ -1,12 +1,12 @@
 resource "azurerm_backup_protected_vm" "backup_association" {
   resource_group_name = var.recovery_vault_rg_name
   recovery_vault_name = var.recovery_vault_name == null ? var.resource_group_name : var.recovery_vault_name
-  source_vm_id        = var.os_type == "Linux" ? azurerm_linux_virtual_machine.vm.id : azurerm_windows_virtual_machine.vm.id
+  source_vm_id        = var.os_type == "Linux" ? azurerm_linux_virtual_machine.vm[0].id : azurerm_windows_virtual_machine.vm[0].id
   backup_policy_id    = var.backup_policy_id
 }
 
 resource "azurerm_role_assignment" "start_stop_role" {
-  scope                = var.os_type == "Linux" ? azurerm_linux_virtual_machine.vm.id : azurerm_windows_virtual_machine.vm.id
+  scope                = var.os_type == "Linux" ? azurerm_linux_virtual_machine.vm[0].id : azurerm_windows_virtual_machine.vm[0].id
   role_definition_name = "Virtual Machine Operator (start/stop)"
   principal_id         = var.backup_operator_principal_id
 }
