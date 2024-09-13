b71fc6c963343ccb72147e00890263608a07498f
Garrukh <79461461+Garrukh@users.noreply.github.com>
2024-09-13 15:01:51 +0200

    Update backup.tf
    default vault rg name conditional corrected

--- backup.tf
+++ backup.tf
@@ -1,6 +1,6 @@
 resource "azurerm_backup_protected_vm" "backup_association" {
-  resource_group_name = var.recovery_vault_rg_name
-  recovery_vault_name = var.recovery_vault_name == null ? var.resource_group_name : var.recovery_vault_name
+  resource_group_name = var.recovery_vault_rg_name == null ? var.resource_group_name : var.recovery_vault_rg_name
+  recovery_vault_name = var.recovery_vault_name
   source_vm_id        = var.os_type == "Linux" ? azurerm_linux_virtual_machine.vm[0].id : azurerm_windows_virtual_machine.vm[0].id
   backup_policy_id    = var.backup_policy_id
 }
